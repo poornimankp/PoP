f7ab9e780495d350711f295792920584d6aedc43
poornimankp <41722422+poornimankp@users.noreply.github.com>
2026-03-08 09:37:25 +0800

    Add files via upload

---
+++ hello_streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Taylor's Student")
+
+st.header("About Me")
+
+st.text("Fact 1: I am a student at Taylor's University.")
+st.text("Fact 2: I am studying Principles of Programming with Python.")
+st.text("Fact 3: I enjoy learning how to build web applications.")
+
+st.success("Welcome to my first Streamlit app!")
